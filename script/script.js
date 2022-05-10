479db5d828a4d704ab746a67fcf752607cce5e25
Gabriel <stanescu.gabriel2001@yahoo.com>
2022-05-10 21:56:15 +0300

    fixed edit button bug
    Signed-off-by: Gabriel <stanescu.gabriel2001@yahoo.com>

--- script/script.js
+++ script/script.js
@@ -3,18 +3,20 @@ function showModal() {
 }
 
 function hideModal() {
-	console.log("t");
 	document.getElementById("modal").style.display = "none";
 }
 
 const addButton = document.getElementById("add_article");
-const editButton = document.getElementById("edit_article");
+const editButtons = document.getElementsByClassName("edit_article");
+
+for (let i = 0; i < editButtons.length; i++) {
+	editButtons[i].addEventListener("click", showModal);
+};
 
 const cancelButton = document.getElementById("cancel");
 const saveButton = document.getElementById("save");
 
 addButton.addEventListener("click", showModal);
-editButton.addEventListener("click", showModal);
 
 cancelButton.addEventListener("click", hideModal);
 saveButton.addEventListener("click", hideModal);
