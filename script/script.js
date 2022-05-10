77d345d53779bcd6d47983d1c395fdbf6c1fddd4
Gabriel <stanescu.gabriel2001@yahoo.com>
2022-05-10 21:44:31 +0300

    fixed modal bug
    Signed-off-by: Gabriel <stanescu.gabriel2001@yahoo.com>

--- script/script.js
+++ script/script.js
@@ -1,19 +1,20 @@
 function showModal() {
-	document.getElementsByClassName("modal")[0].style.display = "block";
+	document.getElementById("modal").style.display = "block";
 }
 
 function hideModal() {
 	console.log("t");
-	document.getElementsByClassName("modal")[0].style.display = "none";
+	document.getElementById("modal").style.display = "none";
 }
 
 const addButton = document.getElementById("add_article");
 const editButton = document.getElementById("edit_article");
+
+const cancelButton = document.getElementById("cancel");
+const saveButton = document.getElementById("save");
+
 addButton.addEventListener("click", showModal);
 editButton.addEventListener("click", showModal);
 
-const cancelButton = document.getElementsByClassName("exit__button")[0];
-console.log(cancelButton);
-const saveButton = document.getElementsByClassName("exit__button")[1];
 cancelButton.addEventListener("click", hideModal);
 saveButton.addEventListener("click", hideModal);
