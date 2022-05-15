63e14f68898216d073ebe1f58067842d41f53a39
GabrielStanescu <stanescu.gabriel2001@yahoo.com>
2022-05-15 21:31:38 +0300

    added main variable
    Signed-off-by: GabrielStanescu <stanescu.gabriel2001@yahoo.com>

--- script/script.js
+++ script/script.js
@@ -20,3 +20,50 @@ addButton.addEventListener("click", showModal);
 
 cancelButton.addEventListener("click", hideModal);
 saveButton.addEventListener("click", hideModal);
+
+
+
+
+// Declare main variable - get main element by id
+const container = document.getElementById('container');
+
+// Fetch the articles list
+function getArticlesFromServer() {
+	fetch('http://localhost:3000/articles')
+		.then(
+			function (response) {
+				if (response.status !== 200) {
+					console.log('Could not fetch the articles. Status Code: ' + response.status);
+					return;
+				}
+
+				response.json().then(function(data) {
+					console.log(data);
+				});
+			}
+		)
+		.catch(function(err) {
+			console.log('Fetch Error', err);
+		});
+}
+
+// Remove articles list if exist
+function removeOldArticlesFromDOM () {
+    // Solution here
+}
+
+function createArticleDOMNode(article) {
+    // Solution here
+}
+
+// Create DOM objects and append them to DOM
+function renderArticles(articles) {
+    
+    removeOldArticlesFromDOM();
+
+    // Create and append articles given as parameter
+
+}
+
+// Get all articles
+getArticlesFromServer();
