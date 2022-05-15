247757177ad2bf7ac14239682fb27391b26636e8
GabrielStanescu <stanescu.gabriel2001@yahoo.com>
2022-05-15 22:14:05 +0300

    rendered articles
    Signed-off-by: GabrielStanescu <stanescu.gabriel2001@yahoo.com>

--- script/script.js
+++ script/script.js
@@ -54,7 +54,86 @@ function removeOldArticlesFromDOM() {
 }
 
 function createArticleDOMNode(article) {
-	// Solution here
+	let title = document.createElement('h1');
+	title.className = 'title';
+	title.innerHTML = article.title;
+
+	let tag = document.createElement('li');
+	tag.className = 'info__item';
+	tag.innerHTML = article.tag;
+
+	let author = document.createElement('span');
+	author.className = 'info__author';
+	author.innerHTML = article.author;
+
+	let date = document.createElement('li');
+	date.className = 'info__item';
+	date.innerHTML = article.date;
+
+	let imgUrl = document.createElement('img');
+	imgUrl.src = article.imgUrl;
+	imgUrl.alt = 'image';
+
+	// let saying = document.createElement('p');
+	// saying.innerHTML = article.saying;
+
+	// let summary = document.createElement('p');
+	// summary.innerHTML = article.summary;
+
+	let content = document.createElement('p');
+	content.className = 'content__text';
+	content.innerHTML = article.content;
+
+	// append title
+	let parent = document.createElement('article');
+	parent.appendChild(title);
+
+	// create ul and append to parent
+	let ul = document.createElement('ul');
+	ul.className = 'info__container';
+
+	let bullet = document.createElement('li');
+	bullet.className = 'info__item';
+	bullet.innerHTML = '•';
+
+	let addedBy = document.createElement('li');
+	addedBy.className = 'info__item';
+	addedBy.innerHTML = 'Added by ';
+	addedBy.appendChild(author);
+
+	ul.appendChild(tag);
+	ul.appendChild(bullet);
+	ul.appendChild(addedBy);
+	ul.appendChild(bullet);
+	ul.appendChild(date);
+
+	parent.appendChild(ul);
+
+	// create buttons
+	let buttonContainer = document.createElement('div');
+	buttonContainer.className = 'modify__container';
+
+	let editB = document.createElement('button');
+	editB.type = 'button';
+	editB.classList.add('modify__button', 'edit_article');
+	editB.innerHTML = 'Edit';
+	buttonContainer.appendChild(editB);
+
+	let deleteB = document.createElement('button');
+	deleteB.type = 'button';
+	deleteB.className = 'modify__button';
+	deleteB.innerHTML = 'Delete';
+	buttonContainer.appendChild(deleteB);
+
+	parent.appendChild(buttonContainer);
+
+	// append image
+	parent.appendChild(imgUrl);
+
+	// append content
+	parent.appendChild(content);
+
+	return parent;
 }
 
 // Create DOM objects and append them to DOM
@@ -63,7 +142,10 @@ function renderArticles(articles) {
 	removeOldArticlesFromDOM();
 
 	// Create and append articles given as parameter
-	console.log(articles);
+	for (let i = 0; i < articles.length; i++) {
+		let article = createArticleDOMNode(articles[i]);
+		main.appendChild(article);
+	}
 }
 
 // Get all articles
